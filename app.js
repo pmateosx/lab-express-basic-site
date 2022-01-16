4ecfd02cae961823dfc0df67c892119122a458b8
Pablo Mateos <pmateos805@gmail.com>
2022-01-16 23:12:20 +0100

    Home & server route

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000 🚀');
+})
