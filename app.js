bc67e9d96e8a3e708a466c0491eead9be59e7770
Pablo Mateos <pmateos805@gmail.com>
2022-01-17 14:40:53 +0100

    Pages and lab done

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get('/home', (req, res) => {
     res.sendFile(__dirname + "/views/home.html")
 })
 
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + "/views/works.html")
+})
+
+app.get('/photo-gallery', (req, res) => {
+    res.sendFile(__dirname + "/views/photo-gallery.html")
+})
+
 app.listen(3000, ()=> {
     console.log('Listening on port 3000 🚀');
 })
